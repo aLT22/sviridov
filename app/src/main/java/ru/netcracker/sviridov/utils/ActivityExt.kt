9b6fbfd4b18e7c45d0ba93970c15016a0eb69f93
Alexey Turkin <altsworks@gmail.com>
2018-10-30 23:04:58 +0300

    small refactoring added few ext methods

--- app/src/main/java/ru/netcracker/sviridov/utils/ActivityExt.kt
+++ app/src/main/java/ru/netcracker/sviridov/utils/ActivityExt.kt
@@ -1,9 +1,13 @@
 package ru.netcracker.sviridov.utils
 
 import android.app.Activity
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
+import android.support.v4.app.FragmentActivity
+import java.io.Serializable
 
 /**
  * Created by Turkin A. on 30/10/2018.
@@ -23,7 +27,7 @@ fun <A : Activity> Activity.startActivityForResult(
     this.startActivityForResult(intent, requestCode)
 }
 
-fun <A : Activity, P : Parcelable> Activity.startActivity(
+fun <A : Activity, P : Parcelable> Activity.startActivityWithParcelables(
     target: Class<A>,
     parameters: Map<String, P>
 ) {
@@ -34,7 +38,18 @@ fun <A : Activity, P : Parcelable> Activity.startActivity(
     this.startActivity(intent)
 }
 
-fun <A : Activity, P : Parcelable> Activity.startActivityForResult(
+fun <A : Activity, S : Serializable> Activity.startActivityWithSerializables(
+    target: Class<A>,
+    parameters: Map<String, S>
+) {
+    val intent = Intent(this, target)
+    val args = Bundle()
+    for (key in parameters.keys) args.putSerializable(key, parameters[key])
+    intent.putExtras(args)
+    this.startActivity(intent)
+}
+
+fun <A : Activity, P : Parcelable> Activity.startActivityForResultWithParcelables(
     target: Class<A>,
     parameters: Map<String, P>,
     requestCode: Int
@@ -45,3 +60,18 @@ fun <A : Activity, P : Parcelable> Activity.startActivityForResult(
     intent.putExtras(args)
     this.startActivityForResult(intent, requestCode)
 }
+
+fun <A : Activity, S : Serializable> Activity.startActivityForResultWithSerializables(
+    target: Class<A>,
+    parameters: Map<String, S>,
+    requestCode: Int
+) {
+    val intent = Intent(this, target)
+    val args = Bundle()
+    for (key in parameters.keys) args.putSerializable(key, parameters[key])
+    intent.putExtras(args)
+    this.startActivityForResult(intent, requestCode)
+}
+
+fun <VM : ViewModel> FragmentActivity.createVM(viewModelTag: String, viewModelClass: Class<VM>) =
+    ViewModelProviders.of(this).get(viewModelTag, viewModelClass)
