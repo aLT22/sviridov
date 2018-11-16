0bc51878f60b673ead32fb6bb1e41e71ce5a9aad
Alexey Turkin <altsworks@gmail.com>
2018-11-17 00:12:42 +0300

    implemented endpoints for weather icons; code refactoring

--- app/src/main/java/ru/netcracker/sviridov/api/MetaWeatherApi.kt
+++ app/src/main/java/ru/netcracker/sviridov/api/MetaWeatherApi.kt
@@ -20,7 +20,19 @@ interface MetaWeatherApi {
     @GET("/api/location/{woeid}/")
     fun getWeather(@Path("woeid") woeID: Long = 44418L): Observable<WeatherResponse>
 
-    /*@GET("/api/location/search/")
-    fun getCities(@Query("query") searchQuery: String): Call<List<CityResponse>>*/
+    companion object {
+
+        const val URL_SNOW = "$BASE_URL/static/img/weather/png/sn.png"
+        const val URL_SLEET = "$BASE_URL/static/img/weather/png/sl.png"
+        const val URL_HAIL = "$BASE_URL/static/img/weather/png/h.png"
+        const val URL_THUNDERSTORM = "$BASE_URL/static/img/weather/png/t.png"
+        const val URL_HEAVY_RAIN = "$BASE_URL/static/img/weather/png/hr.png"
+        const val URL_LIGHT_RAIN = "$BASE_URL/static/img/weather/png/lr.png"
+        const val URL_SHOWERS = "$BASE_URL/static/img/weather/png/s.png"
+        const val URL_HEAVY_CLOUD = "$BASE_URL/static/img/weather/png/hc.png"
+        const val URL_LIGHT_CLOUD = "$BASE_URL/static/img/weather/png/lc.png"
+        const val URL_CLEAR = "$BASE_URL/static/img/weather/png/c.png"
+
+    }
 
 }
--- app/src/main/java/ru/netcracker/sviridov/ui/adapters/recycler/WeatherAdapter.kt
+++ app/src/main/java/ru/netcracker/sviridov/ui/adapters/recycler/WeatherAdapter.kt
@@ -3,12 +3,15 @@ package ru.netcracker.sviridov.ui.adapters.recycler
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.text.Html
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_weather.view.*
 import ru.netcracker.sviridov.R
+import ru.netcracker.sviridov.api.MetaWeatherApi
 import ru.netcracker.sviridov.api.model.ConsolidatedWeather
 import ru.netcracker.sviridov.api.model.WeatherResponse
 
@@ -36,8 +39,9 @@ class WeatherAdapter(
 
         holder.weatherStateName.text = items[position].weatherStateName
         holder.weatherStateAbbr.text = items[position].weatherStateAbbr
+        setWeatherIcon(holder, items[position].weatherStateAbbr ?: "")
 
-        holder.windSpeed.text = items[position].windSpeed.toString()
+        holder.windSpeed.text = items[position].windSpeed?.toInt().toString() + " mph"
 
         holder.tempMin.text = items[position].minTemp?.toInt().toString() + Html.fromHtml("&#8451;")
         holder.temp.text = items[position].theTemp?.toInt().toString() + Html.fromHtml("&#8451;")
@@ -53,6 +57,25 @@ class WeatherAdapter(
 
     companion object {
         const val TAG = "WeatherAdapter"
+    }
+
+    private fun setWeatherIcon(
+        holder: WeatherViewHolder,
+        weatherAbbreviation: String
+    ) {
+        when (weatherAbbreviation) {
+            "sn" -> Picasso.get().load(MetaWeatherApi.URL_SNOW).into(holder.weatherIcon)
+            "sl" -> Picasso.get().load(MetaWeatherApi.URL_SLEET).into(holder.weatherIcon)
+            "h" -> Picasso.get().load(MetaWeatherApi.URL_HAIL).into(holder.weatherIcon)
+            "t" -> Picasso.get().load(MetaWeatherApi.URL_THUNDERSTORM).into(holder.weatherIcon)
+            "hr" -> Picasso.get().load(MetaWeatherApi.URL_HEAVY_RAIN).into(holder.weatherIcon)
+            "lr" -> Picasso.get().load(MetaWeatherApi.URL_LIGHT_RAIN).into(holder.weatherIcon)
+            "s" -> Picasso.get().load(MetaWeatherApi.URL_SHOWERS).into(holder.weatherIcon)
+            "hc" -> Picasso.get().load(MetaWeatherApi.URL_HEAVY_CLOUD).into(holder.weatherIcon)
+            "lc" -> Picasso.get().load(MetaWeatherApi.URL_LIGHT_CLOUD).into(holder.weatherIcon)
+            "c" -> Picasso.get().load(MetaWeatherApi.URL_CLEAR).into(holder.weatherIcon)
+            else -> Log.d(TAG, "Wrong abbreviation")
+        }
     }
 
 }
